b8b6dbf0051797c6848839eb470d1a4f209cf7ef
joaoMarinho94 <joaopaulim94@gmail.com>
2022-08-24 01:30:33 -0300

    feat: add responsivity in app page

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "../Avatar";
 import { Comment } from "../Comments";
 import styles from "./styles.module.css";
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/diego3g.png" />
+          <Avatar src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
